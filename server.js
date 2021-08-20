4135d99a203d4317111cd4f5a1f0e5bf9e2e53a7
LilleLink <simon_0_0_@live.se>
2021-08-20 17:21:30 +0200

    Implements post and get - proxy methods in node server to bypass CORS.

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-const { RSA_NO_PADDING } = require("constants");
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
+const axios = require("axios");
 const app = express();
 
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Proxy methods to bypass CORS
+app.use('/post/', function (req, res) {
+    axios.post('http://localhost:5100/p.json', req.body)
+    .then( response => {
+        res.send(response.data);
+    }).catch (error => {
+        console.log(error);
+    })
+});
+
+app.use('/get/:id/' , function (req, res) {
+    axios.get('http://localhost:5100/p/'+req.params['id']+'.json')
+    .then( response => {
+        res.send(response.data);
+    }).catch(error => {
+        console.error(error);
+    });
+});
+
 app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('/post/', function (req,res) {
-    res.send("hej post lol")
-});
-
-app.get('/get/' , function (req, res) {
-    res.send("hej get lol")
-});
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
-
 
 app.listen(80);
